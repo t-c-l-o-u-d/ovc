ca89642ba0c9b6a9ebd79501cc69e60836c20f59
tcloud <github@varmack.com>
2026-09-03 15:22:06 -0500

    move to scripts

---
+++ .github/scripts/build.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+set -o errexit -o nounset -o pipefail
+
+ARCH="${1:-linux-x86_64}"
+
+echo -e "\n[rustup]"
+rustup default stable
+
+echo -e "\n[cargo-build]"
+cargo build --release
+
+echo -e "\n[strip]"
+strip target/release/ovc
+
+echo -e "\n[artifact]"
+cp target/release/ovc "ovc-${ARCH}"
+
+echo -e "\n[checksum]"
+sha256sum "ovc-${ARCH}" | awk '{print $1}' >"ovc-${ARCH}.sha256"
